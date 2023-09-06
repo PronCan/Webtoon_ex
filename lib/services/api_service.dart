68b2690697bba1412f8d6502290b3d131262f23e
Hochang <twojini98@gmail.com>
2023-09-06 14:48:59 +0900

    ~6.12 모델2개추가, api 2개 추가(디테일, 최근회차10개)

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:webtoon_ex/models/webtoon_detail_model.dart';
+import 'package:webtoon_ex/models/webtoon_episode_model.dart';
 import 'package:webtoon_ex/models/webtoon_model.dart';
 
 class ApiService {
@@ -20,6 +22,31 @@ class ApiService {
         // print(toon.title);
       }
       return webtooInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$_url/$id');
+    final res = await http.get(url);
+    if (res.statusCode == 200) {
+      final webtoon = jsonDecode(res.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse('$_url/$id/episodes');
+    final res = await http.get(url);
+    if (res.statusCode == 200) {
+      final episodes = jsonDecode(res.body);
+      for (var epi in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(epi));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
