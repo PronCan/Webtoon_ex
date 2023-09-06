ff60a6c7d0a560fa10969246a2b0b18c41821144
Hochang <twojini98@gmail.com>
2023-09-06 13:34:57 +0900

    ~6.8 썸네일

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -35,19 +35,16 @@ class HomeScreen extends StatelessWidget {
                   PointerDeviceKind.mouse,
                 },
               ),
-              child: ListView.separated(
-                // ListView.builder
-                // 모든 아이템을 한번에 만드는 대신, 만드는 아이템에 itemBuilder 함수를 실행함
-                // index로 접근이 가능
-                scrollDirection: Axis.horizontal,
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  var webtoon = snapshot.data![index];
-                  return Text(webtoon.title);
-                },
-                separatorBuilder: (context, index) => const SizedBox(
-                  width: 20,
-                ),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Expanded(
+                    // 무한한 높이 해결하기 위해 Listview가 남는 공간 채움
+                    child: makeList(snapshot),
+                  ),
+                ],
               ),
             );
           }
@@ -55,6 +52,58 @@ class HomeScreen extends StatelessWidget {
             child: CircularProgressIndicator(),
           );
         },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      // ListView.builder
+      // 모든 아이템을 한번에 만드는 대신, 만드는 아이템에 itemBuilder 함수를 실행함
+      // index로 접근이 가능
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            // cors 403 error
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
+              ),
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {'Referer': 'https://comic.naver.com'},
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
